1e631401455287f35d89bd67ea606a8a52481ce9
Nikolay Advolodkin <nadvolod@gmail.com>
2023-05-07 13:01:24 -0400

    writing login component testing

--- react-typescript-vite/src/main.tsx
+++ react-typescript-vite/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoginForm from "./components/LoginForm";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <LoginForm
+      onLogin={function (credentials: {
+        username: string;
+        password: string;
+      }): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
   </React.StrictMode>
-)
+);
